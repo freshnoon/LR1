d44103c5a5710ebdb43f24148dda169e02a4a3b1
freshnoon <freshnoon@yandex.ru>
2015-05-06 14:19:04 +0300

    Начальные

---
+++ main.c
@@ -0,0 +1,15 @@
+/* main.c */
+#include <stdio.h>
+
+#include "askname.h"
+
+int main(int argc, char **argv)
+{
+    char first[255], last[255];
+
+    askname(first, last);
+
+    printf("Hello! %s %s!\n", first, last);
+	//lll
+    return 0;
+}
